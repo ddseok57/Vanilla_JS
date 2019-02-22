69c7940d6ac4dad2e41c884cefce80d35d1ad43b
yaaaaang57 <yaaaaang57@naver.com>
2019-02-23 03:57:25 +0900

    mamuri

--- weather.js
+++ weather.js
@@ -11,7 +11,7 @@ function getWeather(lat, lng) {
     .then(function(json) {
         const temperature = json.main.temp;
         const place = json.name;
-        weather.innerText = `${temperature}˚C @ ${place}`
+        weather.innerText = `${temperature}˚C @${place}`
     });
 }
 
